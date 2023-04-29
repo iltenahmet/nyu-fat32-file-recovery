5bf707c8c6f134bf48044d363030f6bc550b81c9
iltenahmet <iltenahmet@gmail.com>
2023-04-29 10:17:41 -0400

    Read info with open and mmap instad of fopen and fread. Use BootEntry struct instad of Fat32Info.

--- nyufile.c
+++ nyufile.c
@@ -10,15 +10,65 @@ To zip specific files:
 zip nyufile.zip Makefile *.h *.c
  */
 
+#include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-typedef struct {
-	unsigned int num_FATs;
-	unsigned int bytes_per_sector;
-	unsigned int sectors_per_cluster;
-	unsigned int reserved_sectors;
-} FAT32Info;
+const int STARTING_CLUSTER = 2;
+const size_t DIR_ENTRY_SIZE = 32;
+
+#pragma pack(push,1)
+typedef struct BootEntry {
+	unsigned char  BS_jmpBoot[3];     // Assembly instruction to jump to boot code
+	unsigned char  BS_OEMName[8];     // OEM Name in ASCII
+	unsigned short BPB_BytsPerSec;    // Bytes per sector. Allowed values include 512, 1024, 2048, and 4096
+	unsigned char  BPB_SecPerClus;    // Sectors per cluster (data unit). Allowed values are powers of 2, but the cluster size must be 32KB or smaller
+	unsigned short BPB_RsvdSecCnt;    // Size in sectors of the reserved area
+	unsigned char  BPB_NumFATs;       // Number of FATs
+	unsigned short BPB_RootEntCnt;    // Maximum number of files in the root directory for FAT12 and FAT16. This is 0 for FAT32
+	unsigned short BPB_TotSec16;      // 16-bit value of number of sectors in file system
+	unsigned char  BPB_Media;         // Media type
+	unsigned short BPB_FATSz16;       // 16-bit size in sectors of each FAT for FAT12 and FAT16. For FAT32, this field is 0
+	unsigned short BPB_SecPerTrk;     // Sectors per track of storage device
+	unsigned short BPB_NumHeads;      // Number of heads in storage device
+	unsigned int   BPB_HiddSec;       // Number of sectors before the start of partition
+	unsigned int   BPB_TotSec32;      // 32-bit value of number of sectors in file system. Either this value or the 16-bit value above must be 0
+	unsigned int   BPB_FATSz32;       // 32-bit size in sectors of one FAT
+	unsigned short BPB_ExtFlags;      // A flag for FAT
+	unsigned short BPB_FSVer;         // The major and minor version number
+	unsigned int   BPB_RootClus;      // Cluster where the root directory can be found
+	unsigned short BPB_FSInfo;        // Sector where FSINFO structure can be found
+	unsigned short BPB_BkBootSec;     // Sector where backup copy of boot sector is located
+	unsigned char  BPB_Reserved[12];  // Reserved
+	unsigned char  BS_DrvNum;         // BIOS INT13h drive number
+	unsigned char  BS_Reserved1;      // Not used
+	unsigned char  BS_BootSig;        // Extended boot signature to identify if the next three values are valid
+	unsigned int   BS_VolID;          // Volume serial number
+	unsigned char  BS_VolLab[11];     // Volume label in ASCII. User defines when creating the file system
+	unsigned char  BS_FilSysType[8];  // File system type label in ASCII
+} BootEntry;
+#pragma pack(pop)
+
+#pragma pack(push,1)
+typedef struct DirEntry {
+	unsigned char  DIR_Name[11];      // File name
+	unsigned char  DIR_Attr;          // File attributes
+	unsigned char  DIR_NTRes;         // Reserved
+	unsigned char  DIR_CrtTimeTenth;  // Created time (tenths of second)
+	unsigned short DIR_CrtTime;       // Created time (hours, minutes, seconds)
+	unsigned short DIR_CrtDate;       // Created day
+	unsigned short DIR_LstAccDate;    // Accessed day
+	unsigned short DIR_FstClusHI;     // High 2 bytes of the first cluster address
+	unsigned short DIR_WrtTime;       // Written time (hours, minutes, seconds
+	unsigned short DIR_WrtDate;       // Written day
+	unsigned short DIR_FstClusLO;     // Low 2 bytes of the first cluster address
+	unsigned int   DIR_FileSize;      // File size in bytes. (0 for directories)
+} DirEntry;
+#pragma pack(pop)
 
 void print_usage_information()
 {
@@ -29,29 +79,34 @@ void print_usage_information()
 	printf("  -R filename -s sha1    Recover a possibly non-contiguous file.\n");
 }
 
-void print_file_system_information(FILE *disk_image)
+BootEntry get_file_system_information(int fd)
 {
-	unsigned char boot_sector[512];
-	fread(boot_sector, 1, 512, disk_image);
+	struct stat sb;
+	if (fstat(fd, &sb) == -1)
+	{
+		perror("fstat error");
+		exit(EXIT_FAILURE);
+	}
 
-	FAT32Info info;
+	void *mapped_data = mmap(NULL, sb.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if (mapped_data == MAP_FAILED)
+	{
+		perror("mmap error");
+		exit(EXIT_FAILURE);
+	}
 
-	const int NUM_FATS_OFFSET = 16;
-	const int BYTES_PER_SECTOR_OFFSET = 11;
-	const int SECTORS_PER_CLUSTER_OFFSET = 13;
-	const int RESERVED_SECTORS_OFFSET = 14;
-
-	info.num_FATs = boot_sector[NUM_FATS_OFFSET];
-	info.bytes_per_sector = boot_sector[BYTES_PER_SECTOR_OFFSET] | (boot_sector[BYTES_PER_SECTOR_OFFSET + 1] << 8);
-	info.sectors_per_cluster = boot_sector[SECTORS_PER_CLUSTER_OFFSET];
-	info.reserved_sectors = boot_sector[RESERVED_SECTORS_OFFSET] | (boot_sector[RESERVED_SECTORS_OFFSET + 1] << 8);
-
-
-	printf("Number of FATs = %u\n", info.num_FATs);
-	printf("Number of bytes per sector = %u\n", info.bytes_per_sector);
-	printf("Number of sectors per cluster = %u\n", info.sectors_per_cluster);
-	printf("Number of reserved sectors = %u\n", info.reserved_sectors);
+	BootEntry boot_entry = *((BootEntry *)mapped_data);
+	return boot_entry;
 }
+
+void print_file_system_information(BootEntry boot_entry)
+{
+	printf("Number of FATs = %u\n", boot_entry.BPB_NumFATs);
+	printf("Number of bytes per sector = %u\n", boot_entry.BPB_BytsPerSec);
+	printf("Number of sectors per cluster = %u\n", boot_entry.BPB_SecPerClus);
+	printf("Number of reserved sectors = %u\n", boot_entry.BPB_RsvdSecCnt);
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -64,6 +119,14 @@ int main(int argc, char *argv[])
 	char *disk_image_arg = argv[1];
 	char *option = argv[2];
 
+	int fd = open(disk_image_arg, O_RDWR);
+	if (fd == -1)
+	{
+		perror("Error opening the disk image.");
+		return 1;
+	}
+	BootEntry boot_entry = get_file_system_information(fd);
+
 	if (strcmp(option, "-i") == 0)
 	{
 		if (argc != 3)
@@ -72,14 +135,7 @@ int main(int argc, char *argv[])
 			return 1;
 		}
 
-		FILE *disk_image = fopen(disk_image_arg, "rb");
-		if (disk_image == NULL)
-		{
-			perror("Error opening the disk image.");
-			return 1;
-		}
-		print_file_system_information(disk_image);
-		fclose(disk_image);
+		print_file_system_information(boot_entry);
 	}
 	else if (strcmp(option, "-l") == 0)
 	{
@@ -89,7 +145,7 @@ int main(int argc, char *argv[])
 			return 1;
 		}
 
-		// List the root directory
+		// list root directory
 	}
 	else if (strcmp(option, "-r") == 0)
 	{
@@ -118,6 +174,8 @@ int main(int argc, char *argv[])
 		print_usage_information();
 		return 1;
 	}
+
+	close(fd);
 
 	return 0;
 }
